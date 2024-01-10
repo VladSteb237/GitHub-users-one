1974d4b7e7076ff9897e0b6b30b5bf08dddafbe6
VladSteb237 <vladsteb@mail.ru>
2024-01-10 19:59:51 +0300

    Change files

--- src/components/Charts/Bar3D.jsx
+++ src/components/Charts/Bar3D.jsx
@@ -9,13 +9,13 @@ import ReactFC from "react-fusioncharts";
 import FusionCharts from "fusioncharts";
 
 // Include the chart type
-import Chart from "fusioncharts/fusioncharts.charts";
+import Bar3d from "fusioncharts/fusioncharts.charts";
 
 // Include the theme as fusion
 import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 
 // Adding the chart and theme as dependency to the core fusioncharts
-ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+ReactFC.fcRoot(FusionCharts, Bar3d, FusionTheme);
 
 // STEP 2 - Chart Data
 
